fda8c11e3c29cabbce96ea77cd947c3b6f90890f
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-07 17:32:37 -0500

    moved db server conn string out og db context class and into program.cs in API project

--- FilmCatalog.API/Program.cs
+++ FilmCatalog.API/Program.cs
@@ -1,4 +1,18 @@
+using FilmCatalog.API.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+IConfigurationRoot config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+            .Build();
+
+string connectionString = config.GetConnectionString("Project") ?? "Error retrieving connection string!";
+
+builder.Services.AddDbContext<FilmCatalogContext>(options =>
+    options.UseSqlServer(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
