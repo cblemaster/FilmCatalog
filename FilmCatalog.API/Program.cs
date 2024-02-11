1433b9f226965fed08d46aba567bb650d698c24e
Brian LeMaster <brian.lemaster@outlook.com>
2024-02-10 20:16:27 -0500

    added placeholder API endpoints

--- FilmCatalog.API/Program.cs
+++ FilmCatalog.API/Program.cs
@@ -16,4 +16,47 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+#region actor endpoints
+app.MapGet("/actor/{actorId:int}", () => { });
+app.MapGet("/actor", () => { });
+app.MapPost("/actor", () => { });
+app.MapPut("/actor/{actorId:int}", () => { });
+app.MapDelete("/actor/{actorId:int}", () => { });
+#endregion
+
+#region category endpoints
+app.MapGet("/category/{categoryId:int}", () => { });
+app.MapGet("/category", () => { });
+app.MapPost("/category", () => { });
+app.MapDelete("/category/{categoryId:int}", () => { });
+#endregion
+
+#region director endpoints
+app.MapGet("/director/{directorId:int}", () => { });
+app.MapGet("/director", () => { });
+app.MapPost("/director", () => { });
+app.MapPut("/director/{directorId:int}", () => { });
+app.MapDelete("/director/{directorId:int}", () => { });
+#endregion
+
+#region film endpoints
+app.MapGet("/film/{filmId:int}", () => { });
+app.MapGet("/film", () => { });
+app.MapGet("/film/favorite", () => { });
+app.MapGet("/film/rare", () => { });
+app.MapGet("/film/fivestar", () => { });
+app.MapPost("/film", () => { });
+app.MapPut("/film/{filmId:int}", () => { });
+app.MapPut("/film/{filmId:int}/actor", () => { });
+app.MapPut("/film/{filmId:int}/categoty", () => { });
+app.MapDelete("/film/{filmId:int}", () => { });
+#endregion
+
+#region format endpoints
+app.MapGet("/format/{formatId:int}", () => { });
+app.MapGet("/format", () => { });
+app.MapPost("/format", () => { });
+app.MapDelete("/format/{formatId:int}", () => { });
+#endregion
+
 app.Run();
